321014506f0fc29be03712ec101fdf993876c73d
Bjorgvin Atli <bjorgvin773@gmail.com>
2019-03-26 11:53:33 +0000

    Björgvin skrifar aðrir hugsa

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const cors = require("cors");
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
+const fs = require('fs');
 app.use(cors());
 
-fetch("https://api.themoviedb.org/3/movie/550?api_key=851a59dab7ff415ab3747e067347ed4e")
-.then(r => r.json())
-.then(j => {
-    console.log()
-})
+fs.readFile('apikey', "utf8", (err, data) => {
+    if(err) console.log("Was not able to read API-key!", err);
+    fetch(`https://api.themoviedb.org/3/movie/550?api_key=${data}`)
+    .then(r => r.json())
+    .then(j => {
+        .map(=>{
+
+        })
+    })
+});
+
+
+
