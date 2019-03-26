721a989f08973c2372c087cadd99a9f835922d79
Guðmundur Helgi Rósuson <GHR@Gumundurs-MacBook-Pro.local>
2019-03-26 13:21:10 +0000

    Gummi contr

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ app.use(cors());
 
 fs.readFile('apikey', "utf8", (err, data) => {
     if(err) console.log("Was not able to read API-key!", err);
-    fetch(`https://api.themoviedb.org/3/movie/550?api_key=${data}`)
+    fetch(`https://api.themoviedb.org/4/list/1?api_key=${data}`, {
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+    }
+})
     .then(r => r.json())
     .then(j => {
-        .map(=>{
-
+        console.log(j)
+        const htmlArray = j.results;
+        .map(movieObj => {
+            return `
+                <div>${movieObj.title}</div>
+            `
+        }).forEach(eventStr=>{
+            document.querySelector(".container").innerHTML += eventStr;
         })
     })
 });
